3f2451403fa05eed4ae7fb00472bd62f0d1023a9
Jainritgithub <riteshjn022@gmail.com>
2024-04-18 17:20:14 +0530

    props-drilling

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Upper from './components/Upper'
+
+const App = () => {
+  return (
+    <div>
+
+      <Upper>
+        Hello World
+      </Upper>
+      <Upper>
+        Hello Worlddebdhebdekdke
+      </Upper>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Upper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Upper = (props) => {
+  return (
+    <div>
+      {props.children.toUpperCase()}
+    </div>
+  )
+}
+
+export default Upper
